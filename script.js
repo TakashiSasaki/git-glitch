de8d9745fcbd5c5bb8ca41f8480c5f22b393e5a8
Glitch (hello-webpage) <none>
2019-01-22 03:50:15 +0000

    ✋🌇 Checkpoint ./script.js:1358164/1708 ./index.html:1358164/2035

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+function 
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
