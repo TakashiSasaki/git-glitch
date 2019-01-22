b8f912967ae15b7d2f4c200d84291699ff2393e5
Glitch (hello-webpage) <none>
2019-01-22 04:00:59 +0000

    ⛵️🏫 Checkpoint ./script.js:1358164/857 ./index.html:1358164/244

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function 
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // [START_EXCLUDE]
-          if (errorCode === 'auth/wrong-password') {
-            alert('Wrong password.');
-          } else {
-            alert(errorMessage);
-          }
-          console.log(error);
-          document.getElementById('quickstart-sign-in').disabled = false;
-          // [END_EXCLUDE]
-        });
-        // [END authwithemail]
+function onLoginButtonClick(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong password.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    document.getElementById('quickstart-sign-in').disabled = false;
+    // [END_EXCLUDE]
+  });
+  // [END authwithemail]
+}
