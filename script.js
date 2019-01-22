5207218b99a52e1ea663abac6d86a801e32daf2d
Glitch (hello-webpage) <none>
2019-01-22 04:11:12 +0000

    🕴🍘 Checkpoint ./script.js:1358164/1443

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    
+    alert("Welcome " + user.email);
+    // ...
+  } else {
+    alert("You are going to logout, bye.");
+    // User is signed out.
+    // ...
+  }
+});
+
+
 function onLoginButtonClick(){
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
