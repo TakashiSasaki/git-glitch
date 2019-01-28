1037f831ce5d5d7acf1da01c22199e8fa2bf2794
Glitch (hello-webpage) <none>
2019-01-28 22:57:59 +0000

    😽👔 Checkpoint ./click.js:1358164/476 ./index.html:1358164/172

--- click.js
+++ click.js
@@ -2,4 +2,20 @@ function onSaveClick(button){
   var defaultDatabase = firebase.database();
   var key = document.getElementById("key").value;
   var ref = firebase.database().ref(key);
+  var value = document.getElementById("value").value;
+  ref.set(value);
+  document.getElementById("log").value = "onSaveClick done";
+}
+
+function onCreateUserClick(button){
+  
+}
+
+function onLoginClick(button){
+}
+
+function onLogoutClick(button){
+}
+
+function onDeleteUserClick(button){
 }
