e91f6714dba7b6371b0d2ad8c13ee7f337a55ff1
Glitch (hello-webpage) <none>
2019-01-29 00:31:59 +0000

    🚟🌞 Checkpoint ./firebase.js:1358164/1456 ./index.html:1358164/46 ./style.css:1358164/33

--- firebase.js
+++ firebase.js
@@ -17,41 +17,15 @@ firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
     var emailVerified = user.emailVerified;
     var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    
-    alert("Welcome " + user.email);
-    // ...
+    var isAnonymous = user.isAnonymous;    
+    document.getElementById("log").value = "Welcome " + user.displayName;
+    document.getElementById("email").value = user.email;
+    document.getElementById("user_id").value = user.uid;
+    document.getElementById("provider_data").value = JSON.stringify(user.providerData);
   } else {
-    alert("You are going to logout, bye.");
-    // User is signed out.
-    // ...
+    document.getElementById("log").value 
+      = "You are going to logout, bye.";
   }
 });
-
-
-function onLoginButtonClick(){
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
-  });
-  // [END authwithemail]
-}
