b372610d1b909391c2e9c7fa8a45beb8afb0105e
Glitch (hello-webpage) <none>
2019-01-31 00:38:23 +0000

    🐗😘 Checkpoint ./sw.js:1358164/494

--- sw.js
+++ sw.js
@@ -26,3 +26,15 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(cacheName) {
+        if ([CACHE_NAME].indexOf(cacheName) === -1) return caches.delete(cacheName);
+      }));
+    })
+  );
+  
+});
+
