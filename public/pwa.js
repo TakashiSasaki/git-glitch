c6bc3a78666e408cf10d82a6a154cb3f2c9242ae
Glitch (gtd-workflow) <none>
2019-03-05 03:39:50 +0000

    🎥🚛 Checkpoint ./public/pwa.js:1358164/1278

--- public/pwa.js
+++ public/pwa.js
@@ -13,23 +13,37 @@ window.addEventListener('load', function() {
     navigator.serviceWorker.register("/sw.js");
     console.log("service worker has been registered.");
     
+    
+    // for push notification via GCM
     navigator.serviceWorker.ready.then(function(registration){
+      if(localStorage.getItem("unsubscribe") === "yes") {
+        registration.pushManager.getSubscription().then(function(subscription){
+          if(subscription) {
+            return subscription.unsubscribe();
+          }
+        }).then(function(){
+          localStorage.removeItem("unsubscribe");          
+        }).catch(function(){
+          localStorage.removeItem("unsubscribe");
+        });
+      }; // unsubscribe existing subscription
       
       console.log('getting subscription ... ');
       return registration.pushManager.subscribe({
         userVisibleOnly: true
       });
     }).then(function(subscription){
-      localStorage.setItem("endpoint", subscription.endpoint);
+      localStorage.setItem("subscription.endpoint", subscription.endpoint);
       console.log("subscription.endpoint : " + subscription.endpoint);
       var auth = btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('auth'))));
-      localStorage.setItem("auth", auth);
+      localStorage.setItem("subscription.auth", auth);
       console.log("subscription.auth : " + auth);
       var p256dh = btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('p256dh'))));
-      localStorage.setItem("p256dh", p256dh);
+      localStorage.setItem("subscription.p256dh", p256dh);
       console.log("subscription.p256dh : " + p256dh);
     }).catch(function(e){
       console.log(e.message);
+      localStorage.setItem("unsubscribe", "yes");
     });
   } else {
     console.log("This browser does not support service worker.");
