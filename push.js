daa3e561b27c7e04c5c254394488ee93d8248419
Glitch (gtd-workflow) <none>
2019-03-05 04:23:53 +0000

    🌨🏩 Checkpoint ./push.js:1358164/929

--- push.js
+++ push.js
@@ -3,25 +3,41 @@
 
 'use strict';
 const fs = require("fs");
-fs.readFile("./.env",'utf8',function(err, text){
+const env = fs.readFileSync("./.env",'utf8');
+const lines = env.split("\n");
+
+var SUBSCRIPTION_ENDPOINT;
+var SUBSCRIPTION_AUTH;
+var SUBSCRIPTION_P256DH;
+var GCM_SERVER_KEY;
+
+for(var i=0; i<lines.length; ++i){
+  var text = lines[i];
+  console.log("text : " + text);
   var x = text.match("SUBSCRIPTION_ENDPOINT=(.*)");
-  if(x) SUBSCRIPTION_ENDPOINT = x;
+  if(x) SUBSCRIPTION_ENDPOINT = x[1];
   var x = text.match("SUBSCRIPTION_AUTH=(.*)");
-  if(x) SUBSCRIPTION_AUTH = x;
+  if(x) SUBSCRIPTION_AUTH = x[1];
   var x = text.match("SUBSCRIPTION_P256DH=(.*)");
-  if(x) SUBSCRIPTION_P256DH = x;
+  if(x) SUBSCRIPTION_P256DH = x[1];
   var x = text.match("GCM_SERVER_KEY=(.*)");
-  if(x) GCM_SERVER_KEY = x;
-});
+  if(x) GCM_SERVER_KEY = x[1];
+};
+
+console.log("SUBSCRIPTION_ENDPOINT : " + SUBSCRIPTION_ENDPOINT);
+console.log("SUBSCRIPTION_AUTH : " + SUBSCRIPTION_AUTH);
+console.log("SUBSCRIPTION_P256DH : " + SUBSCRIPTION_P256DH);
+console.log("GCM_SERVER_KEY : " + GCM_SERVER_KEY);
+
 const push = require('web-push');
 
-const GCM_API_KEY = '**Firebase で調べた API キー**';
+const GCM_API_KEY = GCM_SERVER_KEY;
 push.setGCMAPIKey(GCM_API_KEY);
 
 const data = {
-    'endpoint': '**エンドポイントを指定**',
-    'userAuth': '**鍵生成に使用する乱数指定**',
-    'userPublicKey': '**公開鍵指定**'
+    'endpoint': SUBSCRIPTION_ENDPOINT,
+    'userAuth': SUBSCRIPTION_AUTH,
+    'userPublicKey': SUBSCRIPTION_P256DH
 };
 
 const pushSubscription = {
