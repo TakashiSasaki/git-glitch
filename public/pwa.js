150776f1440dd62cd42527a43cd1404d1ac9dc8f
Glitch (gtd-workflow) <none>
2019-03-05 03:17:54 +0000

    🚿🏔 Checkpoint ./public/pwa.js:1358164/744 ./public/manifest.json:1358164/65

--- public/pwa.js
+++ public/pwa.js
@@ -12,15 +12,21 @@ window.addEventListener('load', function() {
     console.log("registering service worker /sw.js .");
     navigator.serviceWorker.register("/sw.js");
     console.log("service worker has been registered.");
-    navigator.serviceworker.ready.then(function(registration){
+    
+    navigator.serviceWorker.ready.then(function(registration){
       console.log('getting subscription ... ');
-      return registration.pushManager.subscribe({userVisibleOnly: true});
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true
+      });
     }).then(function(subscription){
-      localStorage.setItem("GCM_ENDPOINT", subscription.endpoint);
-      console.log("GCM endpoint : " + subscription.endpoint);
+      localStorage.setItem("endpoint", subscription.endpoint);
+      console.log("subscription.endpoint : " + subscription.endpoint);
       var auth = btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('auth'))));
       localStorage.setItem("auth", auth);
-      
+      console.log("subscription.auth : " + auth);
+      var p256dh = btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('p256dh'))));
+      localStorage.setItem("p256dh", p256dh);
+      console.log("subscription.p256dh : " + p256dh);
     }).catch(function(e){
       console.log(e);
     });
