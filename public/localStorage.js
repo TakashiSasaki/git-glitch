1e0741c4ee3224b5d07dbc9eed2ed0e6b45f0679
Glitch (gtd-workflow) <none>
2019-04-24 00:27:40 +0000

    🎿🚐 Checkpoint ./public/localStorage.js:1358164/409 ./public/muuri.html:1358164/417

--- public/localStorage.js
+++ public/localStorage.js
@@ -1,8 +1,10 @@
 var originalSetItem = localStorage.setItem;
-
 localStorage.setItem = function(k,v) {
+  //console.log("setItem called");
   var event = new Event('setItem');
-  event
-  document.dispatchEvent(event);
-originalSetItem.apply(this, arguments);
+  event.key = k;
+  event.oldValue = localStorage.getItem(k);
+  event.newValue = v;
+  window.dispatchEvent(event);
+  originalSetItem.apply(this, arguments);
 }
