2e01c015dbb97376b3982d8a757623ef944a6ed4
Glitch (gtd-workflow) <none>
2019-04-24 05:45:33 +0000

    🍢🐋 Checkpoint ./.glitch-assets:1358164/783 ./functions/index.js:1358164/456

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,9 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
+/*
 exports.scheduledFunctionCrontab =
 functions.pubsub.schedule('5 11 * * *').onRun((context) => {
     console.log('This will be run every day at 11:05 AM UTC!');
 });
-
+*/
