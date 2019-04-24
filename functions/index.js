72d6a53b97b7223cf3d6a59b61767e2abaaa4c7b
Glitch (gtd-workflow) <none>
2019-04-24 05:56:33 +0000

    👄👾 Checkpoint ./functions/index.js:1358164/147 ./functions/package.json:1358164/670

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const functions = require('firebase-functions');
+const database = require('firebase-database');
 
+console.log(database);
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
 
 /*
