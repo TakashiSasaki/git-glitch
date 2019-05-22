3a615e395eb9facf3d856865aa355da0eac006bc
Glitch (gtd-workflow) <none>
2019-05-22 15:23:29 +0000

    🚐🚡 Checkpoint ./public/iframe.html:1358164/259 ./public/iframe.js:1358164/1210

--- public/iframe.js
+++ public/iframe.js
@@ -1,1 +1,5 @@
-alert(111);
+var iframe = document.getElementsByTagName("iframe")[0];
+var contentWindow = iframe.contentWindow;
+setTimeout(function(){
+  contentWindow.postMessage("hello", "https://gtd-workflow.glitch.me/");
+}, 200);
