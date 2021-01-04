ebdb932c7689a1a6d9a3d9bdff40c54ecb98270e
Glitch (hello-express) <none>
2021-01-04 09:31:53 +0000

    🐴🏇 Checkpoint ./public/spa/favicon.svg:1358164/1513 ./public/spa/authConfig.js:1358164/1894 ./public/spa/authPopup.js:1358164/2700 ./public/spa/authRedirect.js:1358164/2673 ./public/spa/graph.js:1358164/592 ./public/spa/graphConfig.js:1358164/230 ./public/spa/ui.js:1358164/3190

---
+++ public/spa/authConfig.js
@@ -0,0 +1,48 @@
+// Config object to be passed to Msal on creation.
+// For a full list of msal.js configuration parameters, 
+// visit https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+const msalConfig = {
+    auth: {
+        clientId: "908a2d13-cbf0-48a4-b5d1-3413ff8ebe60",
+        authority: "https://login.microsoftonline.com/common",
+        redirectUri: "http://https://outlook-web-app-folder-list.glitch.me/spa/",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {	
+                    return;	
+                }	
+                switch (level) {	
+                    case msal.LogLevel.Error:	
+                        console.error(message);	
+                        return;	
+                    case msal.LogLevel.Info:	
+                        console.info(message);	
+                        return;	
+                    case msal.LogLevel.Verbose:	
+                        console.debug(message);	
+                        return;	
+                    case msal.LogLevel.Warning:	
+                        console.warn(message);	
+                        return;	
+                }
+            }
+        }
+    }
+};
+
+// Add here the scopes that you would like the user to consent during sign-in
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+// Add here the scopes to request when obtaining an access token for MS Graph API
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
