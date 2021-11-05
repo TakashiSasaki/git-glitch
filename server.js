8ab6ae4f968818c634e1ce70f9abd13d6113a108
Glitch (glitch-hello-node) <none>
2021-11-05 08:50:41 +0000

    🍈🍏 Checkpoint ./server.js:1358164/7760 ./public/index.html:1358164/25924

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
 
 const path = require("path");
 
@@ -13,11 +13,16 @@ const fastify = require("fastify")({
 
 // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
-
 // Setup our static files
 fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "node_modules"),
+  prefix: "/node_modules/" // optional: default '/'
+});
+
+fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/" // optional: default '/'
+  //prefix: "/" // optional: default '/'
+  decorateReply: false
 });
 
 // fastify-formbody lets us parse incoming forms
@@ -37,23 +42,26 @@ if (seo.url === "glitch-default") {
 }
 
 /**
-* Our home page route
-*
-* Returns src/pages/index.hbs with data built into it
-*/
-fastify.get("/", function(request, reply) {
-  
+ * Our home page route
+ *
+ * Returns src/pages/index.hbs with data built into it
+ */
+
+fastify.get("/", (request, reply) => {
+  reply.view("/public/index.html");
+});
+
+fastify.get("/index2.html", function(request, reply) {
   // params is an object we'll pass to our handlebars template
   let params = { seo: seo };
-  
+
   // If someone clicked the option for a random color it'll be passed in the querystring
   if (request.query.randomize) {
-    
     // We need to load our color data file, pick one at random, and add it to the params
     const colors = require("./src/colors.json");
     const allColors = Object.keys(colors);
     let currentColor = allColors[(allColors.length * Math.random()) << 0];
-    
+
     // Add the color properties to the params object
     params = {
       color: colors[currentColor],
@@ -61,37 +69,35 @@ fastify.get("/", function(request, reply) {
       seo: seo
     };
   }
-  
+
   // The Handlebars code will be able to access the parameter values and build them into the page
   reply.view("/src/pages/index.hbs", params);
 });
 
 /**
-* Our POST route to handle and react to form submissions 
-*
-* Accepts body data indicating the user choice
-*/
+ * Our POST route to handle and react to form submissions
+ *
+ * Accepts body data indicating the user choice
+ */
 fastify.post("/", function(request, reply) {
-  
   // Build the params object to pass to the template
   let params = { seo: seo };
-  
+
   // If the user submitted a color through the form it'll be passed here in the request body
   let color = request.body.color;
-  
+
   // If it's not empty, let's try to find the color
   if (color) {
     // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
+
     // Load our color data file
     const colors = require("./src/colors.json");
-    
+
     // Take our form submission, remove whitespace, and convert to lowercase
     color = color.toLowerCase().replace(/\s/g, "");
-    
+
     // Now we see if that color is a key in our colors object
     if (colors[color]) {
-      
       // Found one!
       params = {
         color: colors[color],
@@ -99,7 +105,6 @@ fastify.post("/", function(request, reply) {
         seo: seo
       };
     } else {
-      
       // No luck! Return the user value as the error property
       params = {
         colorError: request.body.color,
@@ -107,7 +112,7 @@ fastify.post("/", function(request, reply) {
       };
     }
   }
-  
+
   // The Handlebars template will use the parameter values to update the page with the chosen color
   reply.view("/src/pages/index.hbs", params);
 });
