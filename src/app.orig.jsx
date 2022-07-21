65a6959380a5b675b896dcaf869e6e2f7379bdf0
Glitch (schema-moukaeritai-work) <none>
2022-07-21 09:12:43 +0000

    🐝🎲 Checkpoint ./src/styles/styles.css:1358164/214 ./src/app.jsx:1358164/177 ./src/app.jsx copy:1358164/1706

--- src/app.orig.jsx
+++ src/app.orig.jsx
@@ -38,12 +38,13 @@ export default function Home() {
           <span className="divider">|</span>
           <Link href="/about">About</Link>
         </div>
-        <p>ppp</p>
         <a
           className="btn--remix"
           target="_top"
           href="https://glitch.com/edit/#!/remix/glitch-hello-react"
         >
+          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
+          Remix on Glitch
         </a>
       </footer>
     </Router>
