4ba137f4f58888235072cb72b971920f94504768
Glitch (glitch-hello-node) <none>
2022-11-19 06:19:37 +0000

    🎆🍠 Checkpoint ./public/sign.js:1358164/971 ./public/sign.html:1358164/1733

--- public/sign.js
+++ public/sign.js
@@ -1,4 +1,15 @@
-function generateRegistrationOptions() {
+async function generateRegistrationOptions() {
   const x = await fetch("generateRegistrationOptions", {});
-  document.querySelector("TEXTAREA").value = x;
+  x.json().then((y) => {
+    document.querySelector("#textareaRegistrationOptions").value = JSON.stringify(y);
+  });
+}
+
+async function createSignature(){
+  alert(1);
+  const x = JSON.parse(document.querySelector("#textareaRegistrationOptions").value);
+  const cred = await navigator.credentials.create({
+    publicKey: x
+  });
+  alert(cred);
 }
