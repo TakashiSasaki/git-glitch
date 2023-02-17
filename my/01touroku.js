33fa372629e60713432a3e0229b12c7466c978b1
Glitch (webauthn-codelab-3) <none>
2023-02-17 07:20:40 +0000

    🚖😘 Checkpoint ./my/01touroku.js:1358164/1406 ./my/01touroku.html:1358164/783

--- my/01touroku.js
+++ my/01touroku.js
@@ -1,11 +1,15 @@
 window.addEventListener("load", () => {
   document.querySelector("button").addEventListener("click", (e) => {
     const username = document.querySelector("input").value;
-    fetch("/auth/username", { method: "POST", 
-                             headers:{
-                               "Content-Type":"form/"
-                             },
-                             body: "username=" + username })
+    fetch("/auth/username", {
+      method: "POST",
+      headers: {
+        //"Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json"
+      },
+      //body: "username=" + username,
+      body: JSON.stringify({"username": username});
+    })
       .then((response) => response.text())
       .then((text) => (document.querySelector("textarea").value = text));
   });
