c690a31eca057ced750d17d9e985b2a17b385a68
Glitch (webauthn-codelab-3) <none>
2023-02-17 07:09:18 +0000

    🏪🎓 Checkpoint ./my/01touroku.js:1358164/3518 ./my/01touroku.html:1358164/26

--- my/01touroku.js
+++ my/01touroku.js
@@ -1,7 +1,12 @@
-
-addEventListener("load", () => {
-  addEventListener("click",(e)=>{
-    const username = 
-  } );
-  
+window.addEventListener("load", () => {
+  document.querySelector("button").addEventListener("click", (e) => {
+    const username = document.querySelector("input").value;
+    fetch("/auth/username", { method: "POST", 
+                             headers:{
+                               "Content-Type":"form/"
+                             },
+                             body: "username=" + username })
+      .then((response) => response.text())
+      .then((text) => (document.querySelector("textarea").value = text));
+  });
 });
