eef0f8e2eb56c1a98996b436753d352c1b93d6be
Glitch (glitch-hello-node) <none>
2023-03-09 05:39:53 +0000

    😽👞 Checkpoint ./app-engine-nodejs/index.html:1358164/1288 ./app-engine-nodejs/server.js:1358164/60 ./app-engine-nodejs/route.js:1358164/798

--- app-engine-nodejs/route.js
+++ app-engine-nodejs/route.js
@@ -1,7 +1,12 @@
 const route = (fastify) => {
   fastify.get("/json", (request, reply) => reply.send({ hello: "world" }));
+
+  fastify.get("/get", (request, reply) => {
+    const { CloudMemcacheClient } = require("@google-cloud/memcache").v1;
+    const memcacheClient = new CloudMemcacheClient();
+  });
 };
 
 module.exports = {
-  route: route
+  route: route,
 };
