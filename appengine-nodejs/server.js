157bf04b3b83ec59c4437593bad209f805229068
Glitch (glitch-hello-node) <none>
2023-03-10 03:41:39 +0000

    📮🕶 Checkpoint ./appengine-nodejs/server.js:1358164/1666 ./appengine-nodejs/Makefile:1358164/51 ./appengine-nodejs/app.yaml:1358164/5 ./appengine-nodejs/package.json:1358164/68

--- appengine-nodejs/server.js
+++ appengine-nodejs/server.js
@@ -19,7 +19,7 @@ fastify.register(require("@fastify/view"), {
 fastify.register(require("@fastify/cookie"));
 fastify.register(require("@fastify/session"), {
   secret: process.env.FASTIFY_SESSION_SECRET,
-  cookie: { secure: false, sameSite: "Lax", domain: "glitch.com" },
+  cookie: { secure: false, sameSite: "Lax" },
 });
 
 require("./route").route(fastify);
