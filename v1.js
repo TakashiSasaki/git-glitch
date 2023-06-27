433bc78302b63c3e124845617942904c9c26db29
Glitch (glitch-hello-website) <none>
2023-06-27 04:09:57 +0000

    💕🐉 Checkpoint ./v1.js:1358164/3096

--- v1.js
+++ v1.js
@@ -1,9 +1,40 @@
+class NameContentElement extends HTMLElement {
+  static get observedAttributes(){
+    return ["name", "content"];
+  }
+  
+  connectedCallback() {
+    this.nameInput = document.createElement("input");
+    this.contentInput = document.createElement("input");
+    this.appendChild(this.nameInput);
+    this.appendChild(this.contentInput);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue){
+    if(name="name"){
+      this.nameInput.value = newValue;
+      return;
+    }
+    if(name="content"){
+      this.contentInput = newValue;
+      return;
+    }
+  }
+}
+customElements.define("name-content", NameContentElement);
+
 class MyElement extends HTMLElement {
   connectedCallback() {
     this.textContent = "Hello, Web Components!";
-    this.innerHTML("<input placeholder='abc'></input>");
+    this.innerHTML = "<input placeholder='abc'></input>";
     this.style.backgroundColor = "yellow";
-    this.querySelector("input").value = "abc";
+    
+    document.querySelector("meta").forEach(element =>{
+      const nameContentElement = document.createElement("name-content");
+      
+      nameContentElement.setAttribute("name", element.getAttribute("name"));
+      nameContentElement.setAttribute("content", element.getAttribute("content"));
+    });
   }
 }
 customElements.define("my-element", MyElement);
