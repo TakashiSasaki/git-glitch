2f6fb405276e9aa6d453239e1d0e217214adf36b
Glitch (glitch-hello-website) <none>
2023-06-27 03:58:04 +0000

    🐽💒 Checkpoint ./example1/index.html:1358164/52 ./v1.js:1358164/860

--- v1.js
+++ v1.js
@@ -1,7 +1,9 @@
 class MyElement extends HTMLElement {
-  constructor() {
-    super();
+  connectedCallback() {
     this.textContent = "Hello, Web Components!";
+    this.innerHTML("<input placeholder='abc'></input>");
+    this.style.backgroundColor = "yellow";
+    this.querySelector("input").value = "abc";
   }
 }
 customElements.define("my-element", MyElement);
