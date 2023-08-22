c79701b6484b528a8fe6518571abd385eadc4a2e
Glitch (glitch-hello-react) <none>
2023-08-22 10:24:38 +0000

    🐥⛽️ Checkpoint ./sw.js:1358164/14799 ./sw-common.js:1358164/1098

--- sw.js
+++ sw.js
@@ -1,47 +1,52 @@
 const CACHE_NAME = "20230821T0000";
+const URLS = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/index.css",
+  "/manifest.json",
+  "5a0146b7-8744-49cc-a69d-b38fad10b940.bat",
+  "https://rdfa-streaming-parser.glitch.me/rdfa-streaming-parser.webpack.js",
+  "https://cdn.glitch.global/24ccdc30-220b-4578-a211-8cf3f3e1bcff/favicon.png?v=1692564673448",
+];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      const requests = [
-        "/",
-        "/index.html",
-        "/index.js",
-        "/index.css",
-        "/manifest.json",
-        "https://rdfa-streaming-parser.glitch.me/rdfa-streaming-parser.webpack.js",
-        "https://cdn.glitch.global/24ccdc30-220b-4578-a211-8cf3f3e1bcff/favicon.png?v=1692564673448",
-      ];
-      return Promise.all(
-        requests.map((url) =>
+import "./sw-common.js";
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(
+        URLs.map((url) =>
           fetch(url, { mode: "no-cors" })
-            .then((response) =>
+            .then((URLs) =>
               cache.put(url, response).catch((e) => console.log(e))
             )
             .catch((e) => console.log(e))
         ) //map
-      ); //all
-    }) //then
+      )
+    ) //then
   ); //waitUtil
 }); //addEventListener
 
-self.addEventListener("fetch", (fetchEvent) =>
-  fetchEvent.respondWith(
-    fetch(fetchEvent.request, { mode: fetchEvent.request.mode })
-      .then((response) => {
-        const responseToCache = response.clone();
-        return caches.open(CACHE_NAME).then((cache) =>
-          cache
-            .put(fetchEvent.request, responseToCache)
-            .then(() => response)
-            .catch(() => response)
-        );
-      })
-      .catch((ex) => {
-        return caches
-          .match(fetchEvent.request)
-          .then((response) => response)
-          .catch(() => reponse);
-      })
-  )
+self.addEventListener(
+  "fetch",
+  (fetchEvent) =>
+    fetchEvent.respondWith(
+      fetch(fetchEvent.request, { mode: fetchEvent.request.mode })
+        .then((response) => {
+          const responseToCache = response.clone();
+          return caches.open(CACHE_NAME).then((cache) =>
+            cache
+              .put(fetchEvent.request, responseToCache)
+              .then(() => response)
+              .catch(() => response)
+          );
+        })
+        .catch((ex) =>
+          caches
+            .match(fetchEvent.request)
+            .then((response) => response)
+            .catch(() => reponse)
+        )
+    ) //respondWith
 ); //addEventListener
