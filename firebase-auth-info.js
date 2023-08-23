281547988672333f178bdea6dfbacedb79443042
Glitch (glitch-hello-website) <none>
2023-08-23 19:19:38 +0000

    ⛱👨 Checkpoint ./index.html:1358164/273 ./firebase-auth-info.js:1358164/3437

---
+++ firebase-auth-info.js
@@ -0,0 +1,52 @@
+import { LitElement, html, css } from "https://unpkg.com/lit@2?module";
+
+customElements.define(
+  "firebase-auth-info",
+
+  class extends LitElement {
+    constructor() {
+      super();
+    }
+
+    static get styles() {
+      return css`
+        :host div {
+          background-color: yellow;
+        }
+      `;
+    }
+    
+    get app() {return firebase.auth().app
+    
+
+    connectedCallback() {
+      super.connectedCallback();
+      // インターバルを設定
+      this.intervalId = setInterval(() => {
+        this.data = "Updated data"; // 必要なデータを更新
+        this.requestUpdate(); // 必要に応じてコンポーネントを更新
+      }, 1000); // 1秒ごとに更新
+    }
+
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      // インターバルをクリア
+      clearInterval(this.intervalId);
+    }
+
+    static get properties() {
+      return {
+        greeting: { type: String },
+      };
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      console.log(`${name} changed from ${oldValue} to ${newValue}`);
+      this.greeting = newValue;
+    }
+
+    render() {
+      return html`<div>${this.greeting} hoge</div>`;
+    }
+  }
+);
