86cd20fc8bb9107b5effb257e592f4753756fa23
Glitch (glitch-hello-website) <none>
2023-08-23 08:54:44 +0000

    🐫🏭 Checkpoint ./index.html:1358164/21717 ./init.js:1358164/1639

--- init.js
+++ init.js
@@ -1,18 +1,18 @@
-      // Import the functions you need from the SDKs you need
-      import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-      // TODO: Add SDKs for Firebase products that you want to use
-      // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-      // Your web app's Firebase configuration
-      const firebaseConfig = {
-        apiKey: "AIzaSyCXlAEaeGur3WZ-Jzg2kYtbEPAvfKPYqGo",
-        authDomain: "moukaeritaid.firebaseapp.com",
-        databaseURL:
-          "https://moukaeritaid-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "moukaeritaid",
-        storageBucket: "moukaeritaid.appspot.com",
-        messagingSenderId: "1042140630327",
-        appId: "1:1042140630327:web:6a652f1617c68ad56b9eb2",
-      };
-      // Initialize Firebase
-      const app = initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXlAEaeGur3WZ-Jzg2kYtbEPAvfKPYqGo",
+  authDomain: "moukaeritaid.firebaseapp.com",
+  databaseURL:
+    "https://moukaeritaid-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "moukaeritaid",
+  storageBucket: "moukaeritaid.appspot.com",
+  messagingSenderId: "1042140630327",
+  appId: "1:1042140630327:web:6a652f1617c68ad56b9eb2",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
