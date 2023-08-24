6ba26b22fcba2ab5f8676f98a50caa398ce4908f
Glitch (glitch-hello-website) <none>
2023-08-24 09:17:28 +0000

    🐟💶 Checkpoint ./firebase-user-info.js:1358164/1400

--- firebase-user-info.js
+++ firebase-user-info.js
@@ -4,17 +4,27 @@ customElements.define(
   "firebase-user-info",
   "firebase-auth",
   class extends LitElement {
-    constructor(){
+    constructor() {
       super();
     }
-    
+
     static properties = {
-      userInfo: {type:Object},
-    }//properties
-    
-    
-    render(){}
-    
-    
-  }//class LitElement
+      userInfo: { type: Object },
+    }; //properties
+
+    render() {
+      return html`
+        <fieldset>
+          <legend>
+            <a
+              href="https://firebase.google.com/docs/reference/js/v8/firebase.UserInfo"
+              >firebase.UserInfo</a
+            >
+          </legend>
+          <label>
+          </label>
+        </fieldset>
+      `;
+    }
+  } //class LitElement
 );
