cec0829aee7b0f4022018dc506f69ec01fcfefa4
Glitch (glitch-hello-website) <none>
2023-08-25 02:07:10 +0000

    😜🌺 Checkpoint ./parser-python/parser.py:1358164/11919 ./parser-python/export-one-roundtrip.html:1358164/602 ./parser-python/export-one.html:1358164/106

--- parser-python/parser.py
+++ parser-python/parser.py
@@ -1,22 +1,62 @@
-<!DOCTYPE NETSCAPE-Bookmark-file-1>
-<!-- This is an automatically generated file.
-     It will be read and overwritten.
-     DO NOT EDIT! -->
-<META CONTENT="text/html; charset=UTF-8" HTTP-EQUIV="Content-Type">
-<TITLE>Bookmarks</TITLE>
-<H1>Bookmarks</H1>
-<DL>
-<p>
-<DT><H3<H3 ADD_DATE="1688952193" BOOKMARKS="true" FEEDS="false" FOLDED="true" ID="9e0R8x" LAST_MODIFIED="1689739073">KBC</H3>
-<DT><H3<H3 ADD_DATE="1688952193" BOOKMARKS="true" FEEDS="false" FOLDED="true" ID="mBXM8o" LAST_MODIFIED="1688952235">愛媛大学</H3>
-</DL>
-<p>
-</DL>
-<p>
-<DT><H3<H3 ADD_DATE="1688952193" BOOKMARKS="true" FEEDS="false" FOLDED="true" ID="mBXM8o" LAST_MODIFIED="1688952235">愛媛大学</H3>
-<DT><H3<H3 ADD_DATE="1688952193" BOOKMARKS="true" FEEDS="false" FOLDED="true" ID="mBXM8o" LAST_MODIFIED="1688952235">愛媛大学</H3>
-</DL>
-<p>
-</DL>
-<p>
-</DL><p>
+import sys
+
+def parse_netscape_bookmark_file(file_path):
+    with open(file_path, 'r', encoding="utf-8") as file:
+        content = file.readlines()
+
+    def extract_folders(data, index=0):
+        folders = []
+        current_folder = []
+        while index < len(data):
+            line = data[index].strip()
+            if line.startswith("<DT><H3"):
+                folder_data, index = extract_folders(data, index + 1)
+                current_folder.append({
+                    "folder_start": line,
+                    "content": folder_data
+                })
+            elif line == "</DL>":
+                folders.append(current_folder)
+                index += 2  # Skip the following <p>
+                return folders, index
+            elif line:
+                current_folder.append(line)
+            index += 1
+        folders.append(current_folder)
+        return folders
+
+    header = "".join(content[:content.index("<DL><p>\n")]).strip()
+    folders = extract_folders(content)[0]
+
+    return header, folders
+
+def serialize_folders(folders):
+    result = ""
+    for folder in folders:
+        for item in folder:
+            if isinstance(item, dict):
+                result += item["folder_start"] + "\n"
+                result += "<DL><p>\n"
+                result += serialize_folders(item["content"])
+                result += "</DL>\n<p>\n"
+            else:
+                result += item + "\n"
+    return result
+
+def serialize_to_netscape_format(header, folders):
+    serialized_folders = serialize_folders(folders)
+    return f"{header}\n<DL><p>\n{serialized_folders}</DL><p>"
+
+if __name__ == "__main__":
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+
+    # Parsing the Netscape bookmark file
+    parsed_header, parsed_folders = parse_netscape_bookmark_file(input_file_path)
+
+    # Serializing the parsed structure
+    final_serialized_content = serialize_to_netscape_format(parsed_header, parsed_folders)
+
+    # Writing the serialized content to a file
+    with open(output_file_path, 'w', encoding="utf-8") as file:
+        file.write(final_serialized_content)
