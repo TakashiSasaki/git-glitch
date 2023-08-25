4b87a43e4e4a785b8ac69b7a32ba7935ceeeb974
Glitch (glitch-hello-website) <none>
2023-08-25 02:19:09 +0000

    ✨🏆 Checkpoint ./parser-python/export-one-roundtrip.html:1358164/96 ./parser-python/export-one.html:1358164/4616 ./parser-python/parser.py:1358164/8711

--- parser-python/parser.py
+++ parser-python/parser.py
@@ -1,62 +1,46 @@
-import sys
-
-def parse_netscape_bookmark_file(file_path):
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.readlines()
-
-    def extract_folders(data, index=0):
-        folders = []
-        current_folder = []
-        while index < len(data):
-            line = data[index].strip()
-            if line.startswith("<DT><H3"):
-                folder_data, index = extract_folders(data, index + 1)
-                current_folder.append({
-                    "folder_start": line,
-                    "content": folder_data
-                })
-            elif line == "</DL>":
-                folders.append(current_folder)
-                index += 2  # Skip the following <p>
-                return folders, index
-            elif line:
-                current_folder.append(line)
-            index += 1
-        folders.append(current_folder)
-        return folders
-
-    header = "".join(content[:content.index("<DL><p>\n")]).strip()
-    folders = extract_folders(content)[0]
-
-    return header, folders
+def extract_folders(data, index=0, level=0):
+    folders = []
+    current_folder = []
+    while index < len(data):
+        line = data[index]
+        if line.startswith("<DT><H3"):
+            folder_data, index = extract_folders(data, index + 1, level + 1)
+            current_folder.append({
+                "folder_start": line,
+                "content": folder_data,
+                "level": level
+            })
+        elif line.strip() == "</DL>":
+            folders.append(current_folder)
+            index += 1  # Skip the following <p>
+            return folders, index
+        else:
+            current_folder.append(line.rstrip())
+        index += 1
+    folders.append(current_folder)
+    return folders
 
 def serialize_folders(folders):
     result = ""
     for folder in folders:
         for item in folder:
             if isinstance(item, dict):
-                result += item["folder_start"] + "\n"
-                result += "<DL><p>\n"
+                result += item["folder_start"]
+                if item["level"] == 0:
+                    result += "<DL>\n<p>\n"
+                else:
+                    result += "<DL><p>\n"
                 result += serialize_folders(item["content"])
                 result += "</DL>\n<p>\n"
             else:
                 result += item + "\n"
     return result
 
-def serialize_to_netscape_format(header, folders):
-    serialized_folders = serialize_folders(folders)
-    return f"{header}\n<DL><p>\n{serialized_folders}</DL><p>"
+parsed_header, parsed_folders = parse_netscape_bookmark_file(input_file_path)
+final_serialized_content = serialize_to_netscape_format(parsed_header, parsed_folders)
 
-if __name__ == "__main__":
-    input_file_path = sys.argv[1]
-    output_file_path = sys.argv[2]
+output_file_path_v4 = "/mnt/data/serialized_netscape_bookmarks_v4.html"
+with open(output_file_path_v4, 'w', encoding="utf-8") as file:
+    file.write(final_serialized_content)
 
-    # Parsing the Netscape bookmark file
-    parsed_header, parsed_folders = parse_netscape_bookmark_file(input_file_path)
-
-    # Serializing the parsed structure
-    final_serialized_content = serialize_to_netscape_format(parsed_header, parsed_folders)
-
-    # Writing the serialized content to a file
-    with open(output_file_path, 'w', encoding="utf-8") as file:
-        file.write(final_serialized_content)
+output_file_path_v4
