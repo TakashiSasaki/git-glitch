0ff26fecf234ecc5e992a2a5c49ee21b31b76734
Glitch (glitch-hello-website) <none>
2023-09-19 07:05:20 +0000

    👸💎 Checkpoint ./js.js:1358164/3704

--- js.js
+++ js.js
@@ -1,5 +1,39 @@
+window.addEventListener("load", (event) => {
+  refreshAuthorization();
+  document
+    .querySelector("#getBySinceButton")
+    .addEventListener("click", (event) => {
+      const since = document.querySelector("#sinceInput").value;
+      const count = document.querySelector("#countInput").value;
+      google.script.run
+        .withSuccessHandler((x) => {
+          document.querySelector("textarea").value = x;
+        })
+        .getBySince(offset, count);
+    });
+
+  document
+    .querySelector("#getByOffsetButton")
+    .addEventListener("click", (event) => {
+      const offset = document.querySelector("#offsetInput").value;
+      const count = document.querySelector("#countInput").value;
+      google.script.run
+        .withSuccessHandler((x) => {
+          document.querySelector("textarea").value = x;
+        })
+        .getByOffset(offset, count);
+    });
+});
+
 function refreshAuthorization() {
-  google.script.run.withSuccessHandler((accessToken) => {
-    document.querySelector("accessTokenInput").value = accessToken;
-  }).getUserProperty("accessToken");
+  google.script.run
+    .withSuccessHandler((accessToken) => {
+      document.querySelector("#accessTokenInput").value = accessToken;
+    })
+    .getUserProperty("accessToken");
+  google.script.run
+    .withSuccessHandler((username) => {
+      document.querySelector("#usernameInput").value = username;
+    })
+    .getUserProperty("username");
 }
