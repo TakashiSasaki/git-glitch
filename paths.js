9539f164dfbab55bdc2d89bcde469cadf21a5d3c
Glitch (glitch-hello-website) <none>
2023-09-26 04:10:40 +0000

    🙉🎡 Checkpoint ./sw.js:1358164/22497 ./paths.js:1358164/12670

---
+++ paths.js
@@ -0,0 +1,79 @@
+export const UNITY_PATHS = [
+  "/euvc2023/EhimeWebGL/Build/66037178c278503228e0f5f2511b301a.data",
+  "/euvc2023/EhimeWebGL/Build/dd9ff5803245e1a33787af0195f3d3d8.wasm",
+  "/euvc2023/EhimeWebGL/Build/faf84e8f082175e7c8af023ce113f1ba.js",
+];
+
+export const JPEG_PATHS = [
+  "/euvc2023contents/board/jpg/01HOUBU.jpg",
+  "/euvc2023contents/board/jpg/i_report.jpg",
+  "/euvc2023contents/board/jpg/infinity.jpg",
+  "/euvc2023contents/board/jpg/02KYOUIKU.jpg",
+  "/euvc2023contents/board/jpg/03SHAKYO.jpg",
+  "/euvc2023contents/board/jpg/04RIGAKU.jpg",
+  "/euvc2023contents/board/jpg/05IGAKU.jpg",
+  "/euvc2023contents/board/jpg/06KOUGAKU.jpg",
+  "/euvc2023contents/board/jpg/07NOUGAKU.jpg",
+  "/euvc2023contents/board/jpg/08JYUKEN.jpg",
+  "/euvc2023contents/board/jpg/09OGYA.jpg",
+  "/euvc2023contents/board/jpg/10OSHPITAL.jpg",
+  "/euvc2023contents/board/jpg/11datascience.jpg",
+  "/euvc2023contents/board/jpg/12rstock.jpg",
+  "/euvc2023contents/board/jpg/13newcomer.jpg",
+  "/euvc2023contents/board/jpg/14lr.jpg",
+  "/euvc2023contents/board/jpg/15bousai.jpg",
+  "/euvc2023contents/board/jpg/16SDGS.jpg",
+  "/euvc2023contents/board/jpg/17byouin.jpg",
+  "/euvc2023contents/board/jpg/18kikin.jpg",
+  "/euvc2023contents/board/jpg/19mirainoaidaisei.jpg",
+  "/euvc2023contents/board/jpg/20seikatujyouhou.jpg",
+  "/euvc2023contents/board/jpg/21iikurashi.jpg",
+  "/euvc2023contents/board/jpg/22youchien.jpg",
+  "/euvc2023contents/board/jpg/23shougakko.jpg",
+  "/euvc2023contents/board/jpg/24chuugaku.jpg",
+  "/euvc2023contents/board/jpg/25koukou.jpg",
+  "/euvc2023contents/board/jpg/26tokubetushien.jpg",
+  "/euvc2023contents/board/jpg/27enshuurin.jpg",
+  "/euvc2023contents/board/jpg/28museum.jpg",
+];
+
+export const MP4_PATHS = [];
+
+export const LANDING_PATHS = [
+  "/",
+  "/favicon.ico",
+  "/landing.html",
+  "/landing/nicepage.css",
+  "/landing/nicepage.js",
+  "/landing/jquery-1.9.1.min.js",
+  "/landing/B13_2.jpg",
+  "/landing/eurc-2023.jpg",
+  "/landing/euvc-landing-20230914.jpg",
+];
+
+export const URL_MAPPINGS = [
+  [
+    "/euvc2023contents/board/image/news/01HOUBU.png",
+    "/euvc2023contents/board/jpg/01HOUBU.jpg",
+  ],
+  [
+    "/euvc2023contents/board/image/news/02KYOUIKU.png",
+    "/euvc2023contents/board/jpg/02KYOUIKU.jpg",
+  ],
+  [
+    "/euvc2023contents/board/image/news/25koukou.png",
+    "/euvc2023contents/board/jpg/25koukou.jpg",
+  ],
+  [
+    "/euvc2023contents/board/image/news/i_report.png",
+    "/euvc2023contents/board/jpg/i_report.jpg",
+  ],
+  [
+    "/euvc2023contents/board/image/news/20seikatujyouhou.png",
+    "/euvc2023contents/board/jpg/20seikatujyouhou.jpg",
+  ],
+  [
+    "/euvc2023contents/board/image/news/22youchien.png",
+    "/euvc2023contents/board/jpg/22youchien.jpg",
+  ],
+];
--- sw.js
+++ sw.js
@@ -1,54 +1,23 @@
+import {
+  URL_MAPPINGS,
+  LANDING_PATHS,
+  JPEG_PATHS,
+  MP4_PATHS,
+  UNITY_PATHS,
+} from "./paths.js";
+
 const CACHE_NAME = "20230925T1000";
 
 self.addEventListener("install", (e) => {
   setTimeout(() => {
     caches.open(CACHE_NAME).then(function (cache) {
-      cache
-        .addAll([
-          "/euvc2023/EhimeWebGL/Build/66037178c278503228e0f5f2511b301a.data",
-          "/euvc2023/EhimeWebGL/Build/dd9ff5803245e1a33787af0195f3d3d8.wasm",
-          "/euvc2023/EhimeWebGL/Build/faf84e8f082175e7c8af023ce113f1ba.js",
-        ])
-        .catch((e) => console.log("Caching failed:", e));
+      cache.addAll(UNITY_PATHS).catch((e) => console.log("Caching failed:", e));
     });
   }, 1000);
 
   setTimeout(() => {
     caches.open(CACHE_NAME).then(function (cache) {
-      cache
-        .addAll([
-          "/euvc2023contents/board/jpg/01HOUBU.jpg",
-          "/euvc2023contents/board/jpg/i_report.jpg",
-          "/euvc2023contents/board/jpg/infinity.jpg",
-          "/euvc2023contents/board/jpg/02KYOUIKU.jpg",
-          "/euvc2023contents/board/jpg/03SHAKYO.jpg",
-          "/euvc2023contents/board/jpg/04RIGAKU.jpg",
-          "/euvc2023contents/board/jpg/05IGAKU.jpg",
-          "/euvc2023contents/board/jpg/06KOUGAKU.jpg",
-          "/euvc2023contents/board/jpg/07NOUGAKU.jpg",
-          "/euvc2023contents/board/jpg/08JYUKEN.jpg",
-          "/euvc2023contents/board/jpg/09OGYA.jpg",
-          "/euvc2023contents/board/jpg/10OSHPITAL.jpg",
-          "/euvc2023contents/board/jpg/11datascience.jpg",
-          "/euvc2023contents/board/jpg/12rstock.jpg",
-          "/euvc2023contents/board/jpg/13newcomer.jpg",
-          "/euvc2023contents/board/jpg/14lr.jpg",
-          "/euvc2023contents/board/jpg/15bousai.jpg",
-          "/euvc2023contents/board/jpg/16SDGS.jpg",
-          "/euvc2023contents/board/jpg/17byouin.jpg",
-          "/euvc2023contents/board/jpg/18kikin.jpg",
-          "/euvc2023contents/board/jpg/19mirainoaidaisei.jpg",
-          "/euvc2023contents/board/jpg/20seikatujyouhou.jpg",
-          "/euvc2023contents/board/jpg/21iikurashi.jpg",
-          "/euvc2023contents/board/jpg/22youchien.jpg",
-          "/euvc2023contents/board/jpg/23shougakko.jpg",
-          "/euvc2023contents/board/jpg/24chuugaku.jpg",
-          "/euvc2023contents/board/jpg/25koukou.jpg",
-          "/euvc2023contents/board/jpg/26tokubetushien.jpg",
-          "/euvc2023contents/board/jpg/27enshuurin.jpg",
-          "/euvc2023contents/board/jpg/28museum.jpg",
-        ])
-        .catch((e) => console.log(e));
+      cache.addAll(JPEG_PATHS).catch((e) => console.log(e));
     });
   }, 3000);
 
@@ -56,13 +25,7 @@ self.addEventListener("install", (e) => {
   setTimeout(() => {
     caches.open(CACHE_NAME).then(function (cache) {
       cache
-        .addAll([
-          "/euvc2023contents/board/movie/newsdata/EU08_480.mp4",
-          "/euvc2023contents/board/movie/newsdata/EU04_1080.mp4",
-          "/euvc2023contents/board/movie/newsdata/OGYA_CHAP1.mp4",
-          "/euvc2023contents/board/movie/newsdata/EU03_1080.mp4",
-		  "/euvc2023contents/board/movie/newsdata/EU10_1080.mp4",
-        ])
+        .addAll(MP4_PATHS)
         .catch((e) => console.log("Caching failed:", e));
     });
   }, 10000);
@@ -70,20 +33,8 @@ self.addEventListener("install", (e) => {
 
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      const requests = [
-        "/",
-        "/favicon.ico",
-        "/landing.html",
-        "/landing/nicepage.css",
-        "/landing/nicepage.js",
-        "/landing/jquery-1.9.1.min.js",
-        "/landing/B13_2.jpg",
-        "/landing/eurc-2023.jpg",
-        "/landing/euvc-landing-20230914.jpg",
-      ];
-
       return Promise.all(
-        requests.map((url) =>
+        LANDING_PATHS.map((url) =>
           fetch(url, { mode: "no-cors" })
             .then((response) => cache.put(url, response))
             .catch((e) => console.log(e))
@@ -92,33 +43,6 @@ self.addEventListener("install", (e) => {
     }) //then
   ); //waitUtil
 }); //addEventListener
-
-const URL_MAPPINGS = [
-  [
-    "/euvc2023contents/board/image/news/01HOUBU.png",
-    "/euvc2023contents/board/jpg/01HOUBU.jpg",
-  ],
-  [
-    "/euvc2023contents/board/image/news/02KYOUIKU.png",
-    "/euvc2023contents/board/jpg/02KYOUIKU.jpg",
-  ],
-  [
-    "/euvc2023contents/board/image/news/25koukou.png",
-    "/euvc2023contents/board/jpg/25koukou.jpg",
-  ],
-  [
-    "/euvc2023contents/board/image/news/i_report.png",
-    "/euvc2023contents/board/jpg/i_report.jpg",
-  ],
-  [
-    "/euvc2023contents/board/image/news/20seikatujyouhou.png",
-    "/euvc2023contents/board/jpg/20seikatujyouhou.jpg",
-  ],
-  [
-    "/euvc2023contents/board/image/news/22youchien.png",
-    "/euvc2023contents/board/jpg/22youchien.jpg",
-  ],
-];
 
 const REGEX_PATTERNS = [
   /\/.*\/[a-f0-9]+\.data/,
