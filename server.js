1435a89008c669a63d17529efc78b47994e6ca86
Glitch (glitch-hello-node) <none>
2023-10-02 08:37:44 +0000

    🏩🐚 Checkpoint ./server.js:1358164/291 ./public/index.html:1358164/21

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const path = require("path");
-const fastify = require("fastify")({ logger: true });
+const fastify = require("fastify")({ logger: false });
 const cheerio = require("cheerio");
-const staticServe = require("fastify-static");
+//const staticServe = require("fastify-static");
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
 
 // 静的ファイルを提供する設定
-fastify.register(staticServe, {
-  root: path.join(__dirname, "public"),
-  prefix: "/public/", // オプション
-});
+//fastify.register(staticServe, {
+//  root: path.join(__dirname, "public"),
+//  prefix: "/public/", // オプション
+//});
 
 fastify.post("/extract-text", async (request, reply) => {
   const htmlContent = request.body.htmlContent;
   const $ = cheerio.load(htmlContent);
   const extractedText = $("body").text();
-  return reply.send({ extractedText });
+  return reply.send({ htmlContent });
 });
 
 fastify.get("/", async (request, reply) => {
   return reply.sendFile("index.html"); // 'public' フォルダ内の 'index.html' を指す
 });
 
-const start = async () => {
-  try {
-    await fastify.listen(3000);
-    fastify.log.info(`Server running on http://localhost:3000/`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
+//const start = async () => {
+//  try {
+//    await fastify.listen(3000);
+//    fastify.log.info(`Server running on http://localhost:3000/`);
+//  } catch (err) {
+//    fastify.log.error(err);
+//    process.exit(1);
+//  }
+//};
 
 //start();
 
