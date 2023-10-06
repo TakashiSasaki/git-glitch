31bce2d486582d61bee8a9e8e9fba2e29d729736
Glitch (glitch-hello-website) <none>
2023-10-06 06:21:22 +0000

    🔑🎇 Checkpoint ./flatte/flatten.py:1358164/4353 ./flatte/unflatten.py:1358164/2442 ./flatte/__init__.py:1358164/166

--- flatte/unflatten.py
+++ flatte/unflatten.py
@@ -1,44 +1,28 @@
-def flatten(input_data, path="", delimiter=".", output=None):
+def unflatten(array_data, delimiter='.'):
     """
-    Flattens a nested dict/list structure into a list of tuples.
+    Unflattens a list of (path, value) tuples into a nested dict/list structure.
 
     Parameters:
-    - input_data (dict/list/primitive): The data to flatten.
-    - path (str): The current path being processed (used for recursion).
-    - delimiter (str): The delimiter to use for concatenating keys.
-    - output (list): The list to store the flattened data (used for recursion).
+    - array_data (list): A list of (path, value) tuples to unflatten.
+    - delimiter (str): The delimiter that was used during flattening.
 
     Returns:
-    - list: A list of tuples where each tuple is a (path, value) pair.
-
-    Raises:
-    - ValueError: When a key contains the delimiter.
+    - dict/list/primitive: The unflattened data structure.
     """
-    if output is None:
-        output = []
-
-    if isinstance(input_data, dict):
-        output.append((path, {}))
-
-        for key, value in input_data.items():
-            if not isinstance(key, str):
-                raise ValueError(f"Dictionary key must be a string, found {type(key)} instead.")
-
-            new_key = f"{path}{delimiter}{key}" if path else key
-
-            if delimiter in key:
-                raise ValueError(f"Key '{key}' contains the delimiter '{delimiter}'.")
-
-            flatten(value, new_key, delimiter, output)
-
-    elif isinstance(input_data, list):
-        output.append((path, []))
-
-        for i, value in enumerate(input_data):
-            new_key = f"{path}{delimiter}{i}" if path else str(i)
-            flatten(value, new_key, delimiter, output)
-
-    else:
-        output.append((path, input_data))
-
-    return output
+    root = array_data[0][1]
+    if not isinstance(root, (list, dict)):
+        return root
+    for path, value in array_data[1:]:
+        path_parts = path.split(delimiter)
+        target = root
+        for part in path_parts[:-1]:
+            if isinstance(target, list) and part.isnumeric():
+                part = int(part)
+            target = target[part]
+        last_key = path_parts[-1]
+        if isinstance(target, list) and last_key.isnumeric():
+            last_key = int(last_key)
+            while len(target) <= last_key:
+                target.append(None)
+        target[last_key] = value
+    return root
