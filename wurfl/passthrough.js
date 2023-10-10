83f4c53eb6c8aea96f269df032f05ae4bf6f5b6d
Glitch (glitch-hello-website) <none>
2023-10-10 08:04:48 +0000

    ⛰🚴 Checkpoint ./wurfl/passthrough.js:1358164/2940

--- wurfl/passthrough.js
+++ wurfl/passthrough.js
@@ -1,4 +1,14 @@
-// service-worker.js
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request.url));
+  event.respondWith(
+    fetch(event.request, {
+      mode: "no-cors",
+      headers: {
+        Referer: "https://euvc-pr-ehime-u.glitch.me/",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+      },
+    })
+  );
 });
