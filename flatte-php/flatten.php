0b14cca2f2846e1a93772cc8b39fc4433c996d04
Glitch (glitch-hello-website) <none>
2023-10-12 06:43:31 +0000

    🌾🚿 Checkpoint ./flatte-php/flatten.php:1358164/1096

--- flatte-php/flatten.php
+++ flatte-php/flatten.php
@@ -3,17 +3,21 @@ function flatten($data, $prefix = '', $delimiter = '.') {
     if (!is_array($data) && !is_object($data)) {
         return [$prefix => $data];
     }
-
+  
     $result = [];
     foreach ($data as $key => $value) {
         $new_key = $prefix ? $prefix . $delimiter . $key : $key;
         if (is_array($value) || is_object($value)) {
-            $result += flatten($value, $new_key, $delimiter);
+            if (empty((array)$value)) {
+                $result[$new_key] = [];
+            } else {
+                $result += flatten((array)$value, $new_key, $delimiter);
+            }
         } else {
             $result[$new_key] = $value;
         }
     }
     return $result;
 }
-?>
 
+?>
