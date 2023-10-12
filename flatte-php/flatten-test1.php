4382fa518fda651593a697ca7e15f316b72430b1
Glitch (glitch-hello-website) <none>
2023-10-12 08:30:20 +0000

    🛣😀 Checkpoint ./flatte-php/flatten-test1.php:1358164/7520

--- flatte-php/flatten-test1.php
+++ flatte-php/flatten-test1.php
@@ -3,126 +3,44 @@ use PHPUnit\Framework\TestCase;
 require "flatten.php";
 
 class FlattenToArrayTest extends TestCase {
-    public function testFlattenToArraySimpleObject() {
-        $input = ['a' => ['b' => ['c' => 'd']]];
-        $result = flattenToArray($input);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', '{}'],
-            ['a.b', '{}'],
-            ['a.b.c', 'd']
-        ], $result);
-    }
-
-    public function testFlattenToArrayPrimitiveTypes() {
-        $input = 42;
-        $result = flattenToArray($input);
-        $this->assertEquals([['', 42]], $result);
-
-        $input = 'string';
-        $result = flattenToArray($input);
-        $this->assertEquals([['', 'string']], $result);
-
-        $input = true;
-        $result = flattenToArray($input);
-        $this->assertEquals([['', true]], $result);
-
-        $input = null;
-        $result = flattenToArray($input);
-        $this->assertEquals([['', null]], $result);
-    }
-
-    public function testFlattenToArrayNestingDepth4() {
-        $input = ['a' => ['b' => ['c' => ['d' => ['e' => 1]]]]];
-        $result = flattenToArray($input);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', '{}'],
-            ['a.b', '{}'],
-            ['a.b.c', '{}'],
-            ['a.b.c.d', '{}'],
-            ['a.b.c.d.e', 1]
-        ], $result);
-    }
-  
-    public function testFlattenToArrayMultipleProperties() {
-        $input = [
-            'a' => 1, 
-            'b' => 2, 
-            'c' => [
-                'd' => 3, 
-                'e' => 4
-            ]
-        ];
-        $result = flattenToArray($input);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', 1],
-            ['b', 2],
-            ['c', '{}'],
-            ['c.d', 3],
-            ['c.e', 4]
-        ], $result);
-    }
-  
-    public function testFlattenToArrayMixedTypes() {
+    public function testFlattenToArrayWithNestedObjects() {
         $obj = new stdClass();
-        $obj->d = 3;
-        $obj->e = 4;
-
-        $input = [
-            'a' => 1, 
-            'b' => 2, 
-            'c' => $obj
-        ];
-        $result = flattenToArray($input);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', 1],
-            ['b', 2],
-            ['c', '{}'],
-            ['c.d', 3],
-            ['c.e', 4]
-        ], $result);
-    } 
-  
-    public function testFlattenToArrayRootStdClass() {
-        $obj = new stdClass();
-        $obj->a = 1;
-        $obj->b = 2;
-        $obj->c = ['d' => 3, 'e' => 4];
-
+        $obj->a = new stdClass();
+        $obj->a->b = 1;
         $result = flattenToArray($obj);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', 1],
-            ['b', 2],
-            ['c', '{}'],
-            ['c.d', 3],
-            ['c.e', 4]
-        ], $result);
+        $this->assertEquals([['', '{}'], ['a', '{}'], ['a.b', 1]], $result);
     }
-  
-    public function testFlattenToArrayNested3Levels() {
-        $obj = new stdClass();
-        $obj->a = 1;
-        $obj->b = new stdClass();  // Empty stdClass
-        $obj->c = [
-            'd' => [],  // Empty array
-            'e' => ['f' => 2, 'g' => 3]
-        ];
 
+    public function testFlattenToArrayWithNestedArrays() {
+        $arr = ['x' => ['y' => ['z' => 1]]];
+        $result = flattenToArray($arr);
+        $this->assertEquals([['', '{}'], ['x', '{}'], ['x.y', '{}'], ['x.y.z', 1]], $result);
+    }
+
+    public function testFlattenToArrayWithObjectsUnderArrays() {
+        $arr = ['a' => new stdClass()];
+        $arr['a']->b = 1;
+        $result = flattenToArray($arr);
+        $this->assertEquals([['', '{}'], ['a', '{}'], ['a.b', 1]], $result);
+    }
+
+    public function testFlattenToArrayWithArraysUnderObjects() {
+        $obj = new stdClass();
+        $obj->a = ['b' => 1];
         $result = flattenToArray($obj);
-        $this->assertEquals([
-            ['', '{}'],
-            ['a', 1],
-            ['b', '{}'],
-            ['c', '{}'],
-            ['c.d', '{}'],
-            ['c.e', '{}'],
-            ['c.e.f', 2],
-            ['c.e.g', 3]
-        ], $result);
-    }  
+        $this->assertEquals([['', '{}'], ['a', '{}'], ['a.b', 1]], $result);
+    }
+
+    public function testFlattenToArrayWithEmptyObject() {
+        $obj = new stdClass();
+        $result = flattenToArray($obj);
+        $this->assertEquals([['', '{}']], $result);
+    }
+
+    public function testFlattenToArrayWithEmptyArray() {
+        $arr = [];
+        $result = flattenToArray($arr);
+        $this->assertEquals([['', '{}']], $result);
+    }
 }
 ?>
