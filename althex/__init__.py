7c6dcbd19ee23d75abd6f2416aac526c1cb096ff
Glitch (glitch-hello-website) <none>
2023-11-08 08:01:20 +0000

    🏨🧀 Checkpoint ./althex/test.py:1358164/1399 ./althex/__init__.py:1358164/5086

--- althex/__init__.py
+++ althex/__init__.py
@@ -1,15 +1,49 @@
-# __init__.py
+def determine_case(input_string):
+    # 16進数のアルファベットのみを抽出
+    hex_chars = ''.join(filter(lambda x: x.isalpha(), input_string))
+    # ケースを判断
+    if hex_chars.islower():
+        return False
+    elif hex_chars.isupper():
+        return True
+    elif hex_chars.isalpha() and not hex_chars.islower() and not hex_chars.isupper():
+        raise ValueError("Input string must not contain a mix of uppercase and lowercase letters for hexadecimal characters.")
+    else:
+        return None
 
-def to_althex(hex_string, use_uppercase=False):
+def to_althex(hex_string, use_uppercase=None):
+    # Determine if the output should be uppercase or lowercase
+    # If use_uppercase is None, determine case based on the input string
+    if use_uppercase is None:
+        use_uppercase = determine_case(hex_string)
+    
+    # If the input string contains no hexadecimal characters, we default to lowercase
+    if use_uppercase is None:
+        use_uppercase = False
+
+    # Create the translation map based on the determined case
     hex_to_custom_map = str.maketrans(
-        '0123456789ABCDEF',
-        'GHJKMNPRSTUVWXYZ' if use_uppercase else 'ghjkmnprstuvwxyz'
+        '0123456789ABCDEF' + '0123456789abcdef',
+        'GHJKMNPRSTUVWXYZ' * 2 if use_uppercase else 'ghjkmnprstuvwxyz' * 2
     )
-    return hex_string.upper().translate(hex_to_custom_map)
 
-def from_althex(custom_string, use_uppercase=False):
+    # Translate the standard hex string to the custom hex representation
+    return hex_string.translate(hex_to_custom_map)
+
+
+def from_althex(custom_string, use_uppercase=None):
+    # Determine if the output should be uppercase or lowercase
+    # If use_uppercase is None, determine case based on the input string
+    use_uppercase = determine_case(custom_string) if use_uppercase is None else use_uppercase
+
+    # Create the translation map based on the determined case
     custom_to_hex_map = str.maketrans(
         'GHJKMNPRSTUVWXYZ' if use_uppercase else 'ghjkmnprstuvwxyz',
-        '0123456789ABCDEF'
+        '0123456789ABCDEF' if use_uppercase else '0123456789abcdef'
     )
-    return custom_string.translate(custom_to_hex_map)
+
+    # Translate the custom hex string back to standard hex
+    result = custom_string.translate(custom_to_hex_map)
+
+    # Return the result in the correct case
+    return result.upper() if use_uppercase else result.lower()
--- althex/test.py
+++ althex/test.py
@@ -1,5 +1,6 @@
 import unittest
-from althex import to_althex, from_althex
+import uuid
+from althex import to_althex, from_althex, determine_case
 
 class TestAlthexConversion(unittest.TestCase):
     # to_althex のテスト
@@ -27,6 +28,42 @@ class TestAlthexConversion(unittest.TestCase):
             custom_value = to_althex(hex_value)
             round_trip_value = from_althex(custom_value)
             self.assertEqual(round_trip_value, hex_value)
+
+    def test_uuid_round_trip(self):
+        # UUID を生成
+        original_uuid = uuid.uuid4().hex.upper()
+        # UUID をカスタム16進数に変換
+        custom_uuid = to_althex(original_uuid)
+        # カスタム16進数を元のUUIDに戻す
+        result_uuid = from_althex(custom_uuid)
+        # 元のUUIDと結果が同じであることを確認
+        self.assertEqual(original_uuid, result_uuid)
+
+# determine_case 関数のユニットテストを追加します。
+
+class TestDetermineCase(unittest.TestCase):
+    # 正常なケース
+    def test_determine_case_lowercase(self):
+        self.assertFalse(determine_case("abc123"))
+
+    def test_determine_case_uppercase(self):
+        self.assertTrue(determine_case("ABC123"))
+
+    # コーナーケース
+    def test_determine_case_empty_string(self):
+        self.assertIsNone(determine_case(""))
+
+    def test_determine_case_non_hex_characters(self):
+        self.assertIsNone(determine_case("🙂🚀漢字"))
+
+    def test_determine_case_mixed_case_hex(self):
+        with self.assertRaises(ValueError):
+            determine_case("AbC123")
+
+    # 16進数として解釈しない文字が混在するケース
+    def test_determine_case_mixed_with_non_hex(self):
+        self.assertFalse(determine_case("abc123🙂🚀漢字"))
+        self.assertTrue(determine_case("ABC123🙂🚀漢字"))
 
 if __name__ == '__main__':
     unittest.main()
