df327fc7bc131cefaa5f20febb60be25de71f48f
Glitch (glitch-hello-website) <none>
2023-11-08 09:23:25 +0000

    🍵📍 Checkpoint ./althex.js:1358164/14940

--- althex.js
+++ althex.js
@@ -3,9 +3,9 @@ function toAlthex(hexString, useUppercase) {
   const hexToUpperAlthex = 'GHJKMNPRSTUVWXYZ';
   const hexToLowerAlthex = 'ghjkmnprstuvwxyz';
 
-  // If useUppercase is not provided, determine from the hexString
+  // If useUppercase is not provided (undefined), determine the case from the hexString
   if (useUppercase === undefined) {
-    useUppercase = hexString.toUpperCase() === hexString;
+    useUppercase = hexString.toUpperCase() === hexString && hexString.toLowerCase() !== hexString;
   }
 
   let result = '';
@@ -14,39 +14,42 @@ function toAlthex(hexString, useUppercase) {
     if (!isNaN(index)) {
       result += useUppercase ? hexToUpperAlthex[index] : hexToLowerAlthex[index];
     } else {
-      // If the character is not a valid hexadecimal character, add it as is
-      result += char;
+      throw new Error(`Invalid character for hexadecimal: ${char}`);
     }
   }
   return result;
 }
 
+
+
+
 // Convert custom hexadecimal string back to standard hexadecimal representation
 function fromAlthex(customString, useUppercase) {
   const althexToUpperHex = '0123456789ABCDEF';
   const althexToLowerHex = '0123456789abcdef';
-  
-  // If useUppercase is not provided, determine from the customString
+  const upperAlthexMap = [...'GHJKMNPRSTUVWXYZ'];
+  const lowerAlthexMap = [...'ghjkmnprstuvwxyz'];
+
+  // Determine if the output should be uppercase based on the input string if useUppercase is not defined
   if (useUppercase === undefined) {
-    useUppercase = customString.toUpperCase() === customString;
+    useUppercase = (customString.toUpperCase() === customString);
   }
 
   let result = '';
   for (const char of customString) {
-    const index = useUppercase ? hexToUpperAlthex.indexOf(char) : hexToLowerAlthex.indexOf(char);
+    const index = useUppercase ? upperAlthexMap.indexOf(char) : lowerAlthexMap.indexOf(char);
     if (index !== -1) {
       result += useUppercase ? althexToUpperHex[index] : althexToLowerHex[index];
     } else {
-      // If the character is not in the custom hexadecimal character set, add it as is
-      result += char;
+      throw new Error(`Invalid character for custom hexadecimal: ${char}`);
     }
   }
   return result;
 }
 
 // Example usage:
-const customHex = toAlthex('1A3F'); // should return 'hukz' or 'HUKZ' depending on the case of input
-console.log(customHex);
+const customHex = toAlthex('1A3F', false); // should return 'hukz'
+console.log(customHex); // Output: hukz
 
-const standardHex = fromAlthex(customHex); // should return '1A3F' or '1a3f' depending on the case of input
-console.log(standardHex);
+const standardHex = fromAlthex(customHex, false); // should return '1a3f'
+console.log(standardHex); // Output: 1a3f
