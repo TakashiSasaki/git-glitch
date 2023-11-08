73f19e477f48c940409ecb44cd63ee4e92070cd3
Glitch (glitch-hello-website) <none>
2023-11-08 10:35:24 +0000

    🚤👌 Checkpoint ./althex/test_determine_case.py:1358164/2588 ./althex/to_althex.py:1358164/1728

--- althex/test_determine_case.py
+++ althex/test_determine_case.py
@@ -6,25 +6,37 @@ class TestDetermineCase(unittest.TestCase):
         self.assertTrue(is_upper_althex('GHJKMNPRSTUVWXYZ'))
         self.assertFalse(is_upper_althex('ghjkmnprstuvwxyz'))
         self.assertFalse(is_upper_althex('0123456789'))
+        self.assertFalse(is_upper_althex(''))
         self.assertFalse(is_upper_althex('abcABC'))
+        with self.assertRaises(TypeError):
+            is_upper_althex(123)
 
     def test_is_lower_althex(self):
         self.assertTrue(is_lower_althex('ghjkmnprstuvwxyz'))
         self.assertFalse(is_lower_althex('GHJKMNPRSTUVWXYZ'))
         self.assertFalse(is_lower_althex('0123456789'))
+        self.assertFalse(is_lower_althex(''))
         self.assertFalse(is_lower_althex('abcABC'))
+        with self.assertRaises(TypeError):
+            is_lower_althex(123)
 
     def test_is_upper_stdhex(self):
         self.assertTrue(is_upper_stdhex('ABCDEF'))
         self.assertFalse(is_upper_stdhex('abcdef'))
         self.assertFalse(is_upper_stdhex('0123456789'))
+        self.assertFalse(is_upper_stdhex(''))
         self.assertFalse(is_upper_stdhex('ghjkmnprstuvwxyz'))
+        with self.assertRaises(TypeError):
+            is_upper_stdhex(123)
 
     def test_is_lower_stdhex(self):
         self.assertTrue(is_lower_stdhex('abcdef'))
         self.assertFalse(is_lower_stdhex('ABCDEF'))
         self.assertFalse(is_lower_stdhex('0123456789'))
+        self.assertFalse(is_lower_stdhex(''))
         self.assertFalse(is_lower_stdhex('GHJKMNPRSTUVWXYZ'))
+        with self.assertRaises(TypeError):
+            is_lower_stdhex(123)
 
 if __name__ == '__main__':
     unittest.main()
