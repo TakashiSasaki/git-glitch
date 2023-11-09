f8fb271261d4b4e700d2ac0ba725477c8ec377c4
Glitch (glitch-hello-website) <none>
2023-11-09 03:28:08 +0000

    🐸💿 Checkpoint ./althex/determine_case.py:1358164/997

--- althex/determine_case.py
+++ althex/determine_case.py
@@ -1,37 +1,24 @@
 # Redefine the functions after the reset.
 
+althex_chars = set('GHJKLMNOPQRSTUVWXYZghjklmnopqrstuvwxyz')
+stdhex_chars = set('ABCDEFabcdef')
+
 def is_upper_althex(s):
-    """
-    Check if all althex characters in the string are uppercase (G-Z).
-    """
-    althex_chars = set('GHJKLMNOPQRSTUVWXYZ')
     filtered_chars = set(filter(althex_chars.__contains__, s))
-    return bool(filtered_chars) and filtered_chars.issubset(althex_chars)
+    return bool(filtered_chars) and "".join(filtered_chars).isupper()
 
 
 def is_lower_althex(s):
-    """
-    Check if all althex characters in the string are lowercase (g-z).
-    """
-    althex_chars = set('ghjklmnopqrstuvwxyz')
     filtered_chars = set(filter(althex_chars.__contains__, s))
-    return bool(filtered_chars) and filtered_chars.issubset(althex_chars)
+    return bool(filtered_chars) and "".join(filtered_chars).islower()
 
 
 def is_upper_stdhex(s):
-    """
-    Check if all standard hex characters in the string are uppercase (A-F).
-    """
-    stdhex_chars = set('ABCDEF')
     filtered_chars = set(filter(stdhex_chars.__contains__, s))
-    return bool(filtered_chars) and filtered_chars.issubset(stdhex_chars)
+    return bool(filtered_chars) and "".join(filtered_chars).isupper()
 
 
 def is_lower_stdhex(s):
-    """
-    Check if all standard hex characters in the string are lowercase (a-f).
-    """
     stdhex_chars = set('abcdef')
     filtered_chars = set(filter(stdhex_chars.__contains__, s))
-    return bool(filtered_chars) and filtered_chars.issubset(stdhex_chars)
-
+    return bool(filtered_chars) and "".join(filtered_chars).islower()
