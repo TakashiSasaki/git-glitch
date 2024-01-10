86ccd7d0141506814dc19744e380a658387bfac1
Glitch (korenani) <none>
2024-01-10 07:41:50 +0000

    ⏱🎪 Checkpoint ./build/toggle-component.js:1358164/11595 ./build/index.html:1358164/729 ./build/page-metadata-element.js:1358164/1432 ./build/page-metadata.js:1358164/4619

--- build/toggle-component.js
+++ build/toggle-component.js
@@ -14,7 +14,7 @@ class ToggleComponent extends HTMLElement {
             </style>
             <button id="toggleButton">表示/非表示</button>
             <div id="toggleContent">
-              <slot></slot>  <!-- スロットを追加 -->
+              <slot></slot>
             </div>
           `;
 
@@ -22,7 +22,25 @@ class ToggleComponent extends HTMLElement {
     this.content = this.shadowRoot.querySelector("#toggleContent");
   }
 
+  // 属性の変更を監視するための関数
+  static get observedAttributes() {
+    return ["show-text", "hide-text"];
+  }
+
+  // 属性が変更されたときに呼ばれる関数
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "show-text") {
+      this.showText = newValue;
+    } else if (name === "hide-text") {
+      this.hideText = newValue;
+    }
+    this.updateButtonText();
+  }
+
   connectedCallback() {
+    this.showText = this.getAttribute("show-text") || "Show";
+    this.hideText = this.getAttribute("hide-text") || "Hide";
+
     const isContentVisible =
       localStorage.getItem("isContentVisible") === "true";
     this.content.style.display = isContentVisible ? "block" : "none";
@@ -37,9 +55,7 @@ class ToggleComponent extends HTMLElement {
   }
 
   updateButtonText(isVisible) {
-    this.button.textContent = isVisible
-      ? "Hide Debug Messages"
-      : "Show Debug Messages";
+    this.button.textContent = isVisible ? this.hideText : this.showText;
   }
 }
 
