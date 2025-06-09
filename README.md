c9dd3224cb0a00d87fa3861282311c41077bc393
Takashi SASAKI <takashi316@gmail.com>
2025-06-10 07:38:22 +0900

    Add git subtree add scripts for various projects

--- README.md
+++
@@ -1,2 +0,0 @@
-https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/paste-event-vs-clipboard-api
-https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/gistwriter
---
+++ git-subtree-add.ps1
@@ -0,0 +1,9 @@
+git subtree add --prefix=gistwriter https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/gistwriter main
+
+git subtree add --prefix=paste-event-vs-clipboard-api https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/paste-event-vs-clipboard-api main
+
+git subtree add --prefix=silken-descriptive-decade https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/silken-descriptive-decade main
+
+git subtree add --prefix=thoughtful-glossy-sailboat https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/thoughtful-glossy-sailboat main
+
+git subtree add --prefix=ring-lateral-diadem https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/ring-lateral-diadem main
