33fe376582eed4aebd017b301b916daa53700dc3
Takashi SASAKI <takashi316@gmail.com>
2025-06-10 08:41:30 +0900

    Refactor git subtree commands into a loop for better maintainability

--- git-subtree-add.ps1
+++ git-subtree-add.ps1
@@ -1,24 +1,21 @@
-git subtree add --prefix=gistwriter https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/gistwriter main
+$glitch_base_url = "https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/"
 
-git subtree add --prefix=paste-event-vs-clipboard-api https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/paste-event-vs-clipboard-api main
+$projects = @(
+    "gistwriter",
+    "paste-event-vs-clipboard-api",
+    "silken-descriptive-decade",
+    "thoughtful-glossy-sailboat",
+    "ring-lateral-diadem",
+    "seed-aluminum-fang",
+    "cloud-functions-for-firebase",
+    "korenani",
+    "althex",
+    "thoughtful-shorthaired-avocado",
+    "rdfa-streaming-parser",
+    "bald-hazel-screw"
+    #TODO: このスクリプトの先頭からの９つのgitコマンドにつしてもここにプロジェクト名を移動する。
+)
 
-git subtree add --prefix=silken-descriptive-decade https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/silken-descriptive-decade main
-
-git subtree add --prefix=thoughtful-glossy-sailboat https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/thoughtful-glossy-sailboat main
-
-git subtree add --prefix=ring-lateral-diadem https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/ring-lateral-diadem main
-
-git subtree add --prefix=seed-aluminum-fang https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/seed-aluminum-fang main
-
-git subtree add --prefix=cloud-functions-for-firebase https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/cloud-functions-for-firebase main
-
-git subtree add --prefix=korenani https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/korenani main
-
-git subtree add --prefix=althex https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/althex main
-
-git subtree add --prefix=thoughtful-shorthaired-avocado https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/thoughtful-shorthaired-avocado main
-
-git subtree add --prefix=rdfa-streaming-parser https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/rdfa-streaming-parser main
-
-git subtree add --prefix=bald-hazel-screw https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/bald-hazel-screw main
-
+foreach ($project in $projects) {
+    git subtree add --prefix=$project "$glitch_base_url$project" main
+}
