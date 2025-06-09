54c3b9ba8905ad660c6d70a8c3c9b3b037e599e1
Takashi SASAKI <takashi316@gmail.com>
2025-06-10 08:42:55 +0900

    Comment out unused project names in git subtree add script for clarity

--- git-subtree-add.ps1
+++ git-subtree-add.ps1
@@ -1,19 +1,20 @@
 $glitch_base_url = "https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/"
 
 $projects = @(
-    "gistwriter",
-    "paste-event-vs-clipboard-api",
-    "silken-descriptive-decade",
-    "thoughtful-glossy-sailboat",
-    "ring-lateral-diadem",
-    "seed-aluminum-fang",
-    "cloud-functions-for-firebase",
-    "korenani",
-    "althex",
-    "thoughtful-shorthaired-avocado",
-    "rdfa-streaming-parser",
-    "bald-hazel-screw"
-    #TODO: このスクリプトの先頭からの９つのgitコマンドにつしてもここにプロジェクト名を移動する。
+    #"gistwriter",
+    #"paste-event-vs-clipboard-api",
+    #"silken-descriptive-decade",
+    #"thoughtful-glossy-sailboat",
+    #"ring-lateral-diadem",
+    #seed-aluminum-fang",
+    #"cloud-functions-for-firebase",
+    #"korenani",
+    #"althex",
+    #"thoughtful-shorthaired-avocado",
+    #"rdfa-streaming-parser",
+    #"bald-hazel-screw",
+    "hello-llama-index",
+    "hello-google-keep-api"
 )
 
 foreach ($project in $projects) {
