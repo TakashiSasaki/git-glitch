4510adf2cf0cecc9e2b216aa97a563a2fc2db676
Takashi SASAKI <takashi316@gmail.com>
2025-06-10 16:56:52 +0900

    Remove git subtree add script for obsolete Glitch projects

--- git-subtree-add.sh
+++
@@ -1,9 +0,0 @@
-git subtree add --prefix=gistwriter https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/gistwriter
-
-git subtree add --prefix=paste-event-vs-clipboard-api https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/paste-event-vs-clipboard-api
-
-git subtree add --prefix=silken-descriptive-decade https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/silken-descriptive-decade
-
-git subtree add --prefix=thoughtful-glossy-sailboat https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/thoughtful-glossy-sailboat
-
-git subtree add --prefix=ring-lateral-diadem https://dc7336a4-3887-4118-b631-a722e48b350a@api.glitch.com/git/ring-lateral-diadem
